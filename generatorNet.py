Fixed issues with gram calculation

generatorNet.py
@@ -72,28 +72,32 @@ class GeneratorNet():
         self.clipped_grads = [(tf.clip_by_value(self.grad, -5.0, 5.0), var) for self.grad, var in self.grads]
         self.updateOp = self.optimizer.apply_gradients(self.clipped_grads)
 
-    def train(self, trainingIters = 2, batchSize = 4):
+    def train(self, trainingIters = 20, batchSize = 2):
         self.sess.run(tf.initialize_all_variables())
         print("HERE")
+        
+        
         for iteration in range(trainingIters):
             print("HERE" + str(iteration))
             imageBatch = self.getImages()
             if(iteration%10==0):
 
-                print("Iteration : %s | Loss : %s "%(iteration, self.getLoss(imageBatch, batchSize)))
-
-            feedDict = {self.inputContent : imageBatch.eval(), self.batchSize: int(batchSize), self.currentlyTraining:True }
+                print("Iteration : %s | Loss : %s "%(iteration, self.getLoss(np.random.random((batchSize,256,256,3)), batchSize)))
+            
+            feedDict = {self.inputContent : np.random.random((batchSize,256,256,3)), self.batchSize: int(batchSize), self.currentlyTraining:True }
             self.sess.run(self.updateOp, feed_dict=feedDict)
-
+        self.sess.close()
+        gc.collect()
+        exit()
         #save_path = saver.save(sess, "/tmp/model.ckpt")
 
     def addStyle(self, inputImage):
         image = self.sess.run(self.output, feed_dict={})
 
     def getLoss(self, inputBatch, batchSize =4):
-        currentLoss = self.sess.run(self.loss, feed_dict={self.inputContent : inputBatch.eval(), self.batchSize: int(batchSize), self.currentlyTraining:False})
+        return self.sess.run(self.loss, feed_dict={self.inputContent : inputBatch, self.batchSize: int(batchSize), self.currentlyTraining:False})
 
-    def getImages(self, dir="/Users/matthewsokoloff/Desktop/alejandros crap", batchSize = 4):
+    def getImages(self, dir="/home/matt/repositories/neural_art/images", batchSize = 4):
         filenames = os.listdir(dir)
         filename_queue = tf.train.string_input_producer(filenames, num_epochs=1)
         image_reader = tf.WholeFileReader()

utilities.py
@@ -99,14 +99,27 @@ def buildStyleLoss(model):
     return styleLoss
 
 
-def buildGramMatrix(layer):
-    _, dimX, dimY, num_filters = layer.get_shape()
-    vectorized_maps = tf.reshape(layer, [int(dimX) * int(dimY), int(num_filters)])
+#def buildGramMatrix(layer):
+#    _, dimX, dimY, num_filters = layer.get_shape()
+#    vectorized_maps = tf.reshape(layer, [int(dimX) * int(dimY), int(num_filters)])
 
-    if int(dimX) * int(dimY) > int(num_filters):
-        return tf.matmul(vectorized_maps, vectorized_maps, transpose_a=True)
+#    if int(dimX) * int(dimY) > int(num_filters):
+#        return tf.matmul(vectorized_maps, vectorized_maps, transpose_a=True)
+#    else:
+#        return tf.matmul(vectorized_maps, vectorized_maps, transpose_b=True)
+
+
+def buildGramMatrix(v):
+
+    dim1 = v.get_shape().as_list()
+    v = tf.reshape(v, [-1,dim1[1]*dim1[2],dim1[3]])
+    dim = v.get_shape().as_list()
+
+    if dim1[1] * dim1[2] > dim1[3]:
+        return tf.batch_matmul(tf.reshape(v,[-1, dim[2],dim[1]]),v)
     else:
-        return tf.matmul(vectorized_maps, vectorized_maps, transpose_b=True)
+        return tf.batch_matmul(v,tf.reshape(v,[-1, dim[2], dim[1]]))
+
 
 
 def buildContentLoss(model, correctAnswer=contentData):