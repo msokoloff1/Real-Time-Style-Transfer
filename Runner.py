9eb2a5425ac40ef7ea77ee62ab1dfdd5a39c39c8
msokoloff1 <matthewsokoloff@gmail.com>
2017-01-15 17:35:03 -0500

    Fixed invalid references

--- Runner.py
+++ Runner.py
@@ -7,15 +7,15 @@ import Generator
 
 
 Train.Trainer(savePath = "./savedNets/model.ckpt"
-              ,numIters = 5000
+              ,numIters = 10000
               ,imageShape = (int(1600/8),int(2560/8),3)
               ,contentPath = './images/testingContent1.jpg'
               ,contentLayer = 'conv3_3'
               ,stylePath = './images/testingArt.jpg'
               ,styleLayers =  ['conv1_2','conv2_2','conv3_3', 'conv4_1', 'conv5_1']
               ,styleWeights = [0.2      ,0.2     , 0.3     , 0.3       , 0.2      ]
-              ,TVNormLossWeight =  0.0000
-              ,styleLossWeight=0.5
+              ,TVNormLossWeight =  0.0001
+              ,styleLossWeight=0.8
               ,contentLossWeight=0.05)
 
 
@@ -23,4 +23,4 @@ with tf.Session() as sess:
     testImageShape = (int(1600/4), int(2560/4), 3)
     tester = Test.LoadedModel(sess,testImageShape, modelPath = "./savedNets/model.ckpt")
     
-    tester.predict("./testImages",pathIsDir = True)
+    tester.predict("./testImages", pathIsDir = True)
--- Test.py
+++ Test.py
@@ -1,36 +1,42 @@
 import tensorflow as tf
 import Generator
-    
+import os
+import utilities as utils
+import time 
+import numpy as np
+
 class LoadedModel():
     def __init__(self,sess, imageShape, modelPath):
-        with tf.Session() as sess:
-            self.sess  = sess
-            self.imageShape = imageShape
-            self.gen = Generator.GeneratorNet(self.sess, self.imageShape)
-            saver = tf.train.Saver()
-            try:
-                
-                saver.restore(sess, modelPath)  #"./modelNight.ckpt"
-                print("Successfully Restored Model") 
-            except:
-                print("No model available for restoration")
+        
+        self.sess  = sess
+        self.imageShape = imageShape
+        self.gen = Generator.GeneratorNet(self.sess, self.imageShape)
+        saver = tf.train.Saver()
+        try:
+            
+            saver.restore(sess, modelPath)  #"./modelNight.ckpt"
+            print("Successfully Restored Model") 
+        except:
+            print("No model available for restoration")
         
     
-    def predict(testPath,pathIsDir):
+    def predict(self, testPath,pathIsDir):
         if(pathIsDir):
             #dir = "/home/matt/repositories/fastStyleTransfer/testImages"
             for element in os.listdir(testPath):
                 path = testPath + "/" + element
                 start = time.time()
-                testImg = np.array(utils.loadImage(path, imageShape))
-                img = self.gen.predict(testImg.reshape(1,imageShape[0],imageShape[1],imageShape[2]))
-                utils.showImage(img,imageShape, "Night"+ str(element))
+                testImg = np.array(utils.loadImage(path, self.imageShape))
+                img = self.gen.predict(testImg.reshape(1,self.imageShape[0],self.imageShape[1],self.imageShape[2]))
+                utils.showImage(img,self.imageShape, "Night"+ str(element))
                 elasped = time.time() - start
                 print("Made Prediction for: %s | Time taken : %s"%(element, elasped))
         else:
             start = time.time()
             testImg = np.array(utils.loadImage(testPath, self.imageShape))
             img = self.gen.predict(testImg.reshape(1,self.imageShape[0],self.imageShape[1],self.imageShape[2]))
-            utils.showImage(img,imageShape, "Night"+ str(element))
+            utils.showImage(img,self.imageShape, "NewTrials"+ str(element))
             elasped = time.time() - start
             print("Made Prediction | Time taken : %s"%(element, elasped))
+            
+    
